2e021d95d8b91b4f74461abbbd17ff075c16b591
Tony Simpson <Anthony.Simpson@mhr.co.uk>
2023-04-14 14:56:18 +0100

    jhiok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,14 +7,19 @@ namespace TestAppOne.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
     {
+        ViewBag.Message = _configuration.GetValue<string>("TestValue");
         return View();
     }
 
